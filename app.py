7d29d3091f11050c5ba1e4cea24c80000c358d29
Bernhard Bock <bernhard@bock.nu>
2016-07-28 18:34:33 +0200

    add authentication, IPv6, variable hostname and error handling

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ the API gateway endpoint on each IP address change.
 """
 
 from chalice import Chalice
+from chalice import BadRequestError
 import boto3
 import re
 
@@ -14,8 +15,9 @@ app = Chalice(app_name='route53-dyndns')
 app.debug = False
 
 config = {
-    'ZoneId': 'AWS-ZoneID',
-    'Hostname': 'fqdn.of.ddns.client.example.net'
+    'ZoneId': 'AWS-Route53-ZoneID',
+    'Username': 'ddns-username',
+    'Password': 'ddns-password'
 }
 
 
@@ -23,26 +25,62 @@ config = {
 def index():
     """Main function"""
     client = boto3.client('route53')
-    client_ip = re.split(",?", app.current_request.headers['X-Forwarded-For'])[0]
+
+    # check for mandatory hostname
+    if 'hostname' in app.current_request.query_params:
+        hostname = app.current_request.query_params['hostname']
+    else:
+        raise BadRequestError('Missing hostname!')
+
+    # Authentication
+    if ('username' in app.current_request.query_params and
+            'password' in app.current_request.query_params):
+        username = app.current_request.query_params['username']
+        password = app.current_request.query_params['password']
+    else:
+        raise BadRequestError("Invalid credentials")
+    if username != config['Username'] or password != config['Password']:
+        raise BadRequestError("Invalid credentials")
+
+    if 'ip4addr' in app.current_request.query_params:
+        ip4addr = app.current_request.query_params['ip4addr']
+    else:
+        # fallback to X-Forwarded-For if client submits no IP in query params
+        ip4addr = re.split(",?", app.current_request.headers['X-Forwarded-For'])[0]
+
+    # define update for IPv4. this is mandatory
+    changes = [{
+        'Action': 'UPSERT',
+        'ResourceRecordSet': {
+            'Name': hostname,
+            'Type': 'A',
+            'TTL': 5,
+            'ResourceRecords': [{
+                'Value': ip4addr
+            }]
+        }
+    }]
+
+    # add update for IPv6, if defined
+    if 'ip6addr' in app.current_request.query_params:
+        ip6addr = app.current_request.query_params['ip6addr']
+        changes.append({
+            'Action': 'UPSERT',
+            'ResourceRecordSet': {
+                'Name': hostname,
+                'Type': 'AAAA',
+                'TTL': 5,
+                'ResourceRecords': [{
+                    'Value': ip6addr
+                }]
+            }
+        })
+
     client.change_resource_record_sets(
         HostedZoneId=config['ZoneId'],
         ChangeBatch={
-            'Comment': 'Update by AWS Lambda function',
-            'Changes': [
-                {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': config['Hostname'],
-                        'Type': 'A',
-                        'TTL': 5,
-                        'ResourceRecords': [
-                            {
-                                'Value': client_ip
-                            }
-                        ]
-                    }
-                }
-            ]
+            'Comment': 'Update by AWS Lambda function for DDNS',
+            'Changes': changes
         }
     )
     return "Update successful"
